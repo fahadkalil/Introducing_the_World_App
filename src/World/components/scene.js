bb1417414484e50e280a238f65be28a35688b5d5
Fahad Kalil <4945023+fahadkalil@users.noreply.github.com>
2025-10-22 21:42:56 -0300

    iluminacao e neblina

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -5,6 +5,14 @@ function createScene() {
   scene.background = new THREE.Color(0x21272e);
   scene.add(new THREE.GridHelper());
 
+  // iluminação
+  const light = new THREE.DirectionalLight();
+  light.position.set(5, 5, 4).normalize();
+  scene.add(light);
+
+  // neblina (névoa)
+  scene.fog = new THREE.Fog(0xb4e137, 8, 20);
+
   return scene;
 }
 
